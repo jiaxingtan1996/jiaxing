b8fd6feb6927ff9a528327ffd6c0e84605eae8a5
Jiachin Tam <jiachintam@jiachindembp.dhcp.swmed.org>
2026-04-19 17:49:46 -0500

    Add new post

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/posts/protein-language-model.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fprotein-language-model.mdx&astroContentModuleFlag=true")],
+["src/content/posts/welcome-to-my-blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fwelcome-to-my-blog.mdx&astroContentModuleFlag=true")]]);
+		
