4f11242baf55f24efff07189a7159939d0d717de
Jiachin Tam <jiachintam@jiachindembp.dhcp.swmed.org>
2026-04-19 17:09:05 -0500

    Add new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jiaxing-tan.netlify.app/',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
